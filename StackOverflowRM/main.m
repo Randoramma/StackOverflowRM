90fb8222f86def5ff50ff7cb20eed31eb51e9cc8
Randy McLain <Randoramma@me.com>
2015-05-11 22:31:12 -0700

    Initial Commit

---
+++ StackOverflowRM/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  StackOverflowRM
+//
+//  Created by Randy McLain on 5/11/15.
+//  Copyright (c) 2015 Randy McLain. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+  @autoreleasepool {
+      return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+  }
+}
